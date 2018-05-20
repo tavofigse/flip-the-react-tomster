733e899c69c3c057eca0280b849de4b01ad99f2f
Gustavo <gfasequeira@gmail.com>
2018-05-19 22:20:56 -0300

    remove animation on Footer component.

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,16 +1,6 @@
 import * as React from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import * as Variables from '../../styles/variables';
-
-const rotate360 = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
-`;
 
 const FooterContainer = styled.div`
   text-align: center;
@@ -18,14 +8,13 @@ const FooterContainer = styled.div`
 
 const FooterLabel = styled.p`
   color: ${Variables.COLOR_BLACK};
-  font-family: ${Variables.FONT_FOOTER};
+  font-family: ${Variables.FONT_SECONDARY};
   font-weight: bold;
   margin-left: 3.6vw;
   margin-top: 15vw;
 `;
 
 const Heart = styled.p`
-  animation: ${rotate360} 2s linear infinite;
   margin-left: 3.2vw;
   font-size: 25px;
 `;
