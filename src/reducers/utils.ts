0128b8ddd5536219fb2b4b2f16588c61909b205e
Gustavo <gfasequeira@gmail.com>
2018-08-31 20:48:58 -0300

    Little fix on generateCards.

--- src/reducers/utils.ts
+++ src/reducers/utils.ts
@@ -3,9 +3,10 @@ import { figures, range, shuffle  } from '../containers/utils';
 import { ICard } from '../models/game/Card';
 
 export function genereateCards(size: number, figure: string = "tomster"): List<ICard> {
+    const amountOfCards = size * size / 2;
     const amountOFFigures = figures[figure];
-
     const cards = shuffle(range(1, amountOFFigures))
+        .slice(0, amountOfCards)
         .map(n => ({
             cardId: String(n),
             figure,
