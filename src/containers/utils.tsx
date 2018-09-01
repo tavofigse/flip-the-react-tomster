560520f3c236185da81263e19523db17e45b3332
Gustavo <gfasequeira@gmail.com>
2018-08-31 23:43:54 -0300

    double shuffle for real shuffle

--- src/containers/utils.tsx
+++ src/containers/utils.tsx
@@ -1,5 +1,4 @@
 import { List, Range } from 'immutable';
-import { ICard } from '../models/game/Card';
 
 export const figures = {
     dog: 32,
@@ -10,7 +9,7 @@ export function range(min: number, max: number): List<number> {
     return List(Range(min, max));
 }
 
-export function shuffle(array: List<ICard>): List<ICard> {
+export function shuffle<T>(array: List<T>): List<T> {
     return array.sortBy(Math.random).toList();
   }
   
